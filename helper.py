3425e9a14356ff25cfb84f004591bf04b24b95b6
Милокум Павел <ya-shev4@yandex.ru>
2022-07-10 19:31:56 +0300

    First commit

---
+++ helper.py
@@ -0,0 +1,176 @@
+import argparse
+import CGPCLI
+import keyring
+import getpass
+from typing import *
+import base64
+import binascii
+
+
+class CGPROHelper:
+    __address: str = ''
+    __login: str = ''
+    __password: str = ''
+    __server: CGPCLI.Server
+    # noinspection SpellCheckingInspection
+    __full_rights: str = 'cdilrsw'
+
+    def __init__(self, login: str, password: str, address: str = '10.16.252.45') -> None:
+        self.__login = login
+        self.__password = password
+        self.__address = address
+        self.__server = CGPCLI.Server(address)
+
+    def __connect(self) -> None:
+        self.__server.connect()
+        self.__server.login(self.__login, self.__password)
+
+    def __disconnect(self) -> None:
+        self.__server.disconnect()
+
+    @staticmethod
+    def get_function(method: Callable) -> Any:
+        def wrapper(self, *args) -> dict:
+            self.__connect()
+            result = method(self, *args)
+            self.__disconnect()
+            return result
+
+        return wrapper
+
+    @staticmethod
+    def get_rights():
+        return CGPROHelper.__full_rights
+
+    @staticmethod
+    def mailbox_name_quoted(mailbox_name: str):
+        if mailbox_name.startswith('\"') and mailbox_name.endswith('\"'):
+            return mailbox_name
+        return '\"{}\"'.format(mailbox_name)
+
+    @staticmethod
+    def mailbox_alias(user_name, mailbox_name_raw):
+        return '~{}/{}'.format(user_name, mailbox_name_raw)
+
+    @get_function
+    def get_domains(self) -> list:
+        return self.__server.list_domains()['body']
+
+    @get_function
+    def get_users(self, domain_name: str) -> list:
+        return list(self.__server.list_accounts(domain_name)['body'].keys())
+
+    @get_function
+    def get_groups(self, domain_name: str) -> list:
+        return self.__server.list_groups(domain_name)['body']
+
+    @get_function
+    def get_group(self, group_name: str, domain_name: str) -> dict:
+        return self.__server.get_group(group_name + '@' + domain_name)['body']
+
+    @get_function
+    def get_mailbox_rights(self, account_name: str, mailbox_name: str, auth_account_name: str) -> str:
+        mailbox_name = CGPROHelper.mailbox_name_quoted(mailbox_name)
+        rights = self.__server.get_mailbox_ACL(account_name, mailbox_name, None, None)['body']
+        for key in rights:
+            if key in auth_account_name:
+                return rights[key]
+        return ''
+
+    @get_function
+    def get_mailboxes(self, account_name) -> dict:
+        return self.__server.list_mailboxes(account_name)['body']
+
+    @get_function
+    def set_mailbox_rights(self, account_name: str, mailbox_name: str, auth_account_name: str):
+        mailbox_name = CGPROHelper.mailbox_name_quoted(mailbox_name)
+        new_acl = {auth_account_name: CGPROHelper.get_rights()}
+        return self.__server.set_mailbox_ACL(account_name, mailbox_name, new_acl)
+
+    @get_function
+    def set_mailbox_aliases(self, account_name: str, new_aliases: dict):
+        return self.__server.set_mailbox_aliases(account_name, new_aliases)
+
+    def get_all_users_by_domains(self) -> dict:
+        return {domain: [user for user in self.get_users(domain)] for domain in self.get_domains()}
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--newpass', required=False, help='Set new password', action='store_true')
+    arguments = parser.parse_args()
+    return arguments
+
+
+def test_domains_and_users():
+    password: str = keyring.get_password(system_name, username)
+    helper: CGPROHelper = CGPROHelper(username, password)
+    print('DOMAINS AND USERS')
+    for d in helper.get_domains():
+        print(d)
+        print(helper.get_users(d))
+    print('DOMAINS AND GROUPS')
+    for d in helper.get_domains():
+        print(d)
+        for g in helper.get_groups(d):
+            print(g)
+            print(helper.get_group(g, d))
+
+
+def test_get_set_acl2(calendar_username: str = 'testpublic@energospb.ru'):
+    password: str = keyring.get_password(system_name, username)
+    helper: CGPROHelper = CGPROHelper(username, password)
+    test_user2 = 'testreceiver@energospb.ru'
+    mailboxes = helper.get_mailboxes(calendar_username)
+    calendars_to_share = []
+    for key, value in mailboxes.items():
+        try:
+            # noinspection SpellCheckingInspection
+            if base64.b64decode(key).decode('utf-16BE') != 'Календарь':
+                if ('Class' in value) and (value['Class'] == 'IPF.Appointment'):
+                    calendars_to_share.append(key)
+        except binascii.Error:
+            pass
+    # TODO - change test_user2 to all users in all domains
+    for calendar in calendars_to_share:
+        rights = helper.get_mailbox_rights(calendar_username, calendar, test_user2)
+        if rights != helper.get_rights():
+            helper.set_mailbox_rights(calendar_username, calendar, test_user2)
+            helper.set_mailbox_aliases(test_user2, {calendar: helper.mailbox_alias(calendar_username, calendar)})
+
+
+def test_get_set_acl():
+    password: str = keyring.get_password(system_name, username)
+    helper: CGPROHelper = CGPROHelper(username, password)
+    # print(helper.get_mailboxes('testpublic@energospb.ru'))
+    # noinspection SpellCheckingInspection
+    test_user1 = 'testpublic@energospb.ru'
+    test_user2 = 'testreceiver@energospb.ru'
+    # test_user3 = 'boyko.anastasiya@energospb.ru'
+    # noinspection SpellCheckingInspection
+    knights_hall_mailbox_raw = '&BCAESwRGBDAEQARBBDoEOAQ5- &BDcEMAQ7-'
+    knights_hall_mailbox_name = '\"{}\"'.format(knights_hall_mailbox_raw)
+    knights_hall_alias = '~{}/{}'.format(test_user1, knights_hall_mailbox_raw)
+    knights_hall_dict = {knights_hall_mailbox_raw: knights_hall_alias}
+    print('Old rights: ')
+    print(helper.get_mailbox_rights(test_user1, knights_hall_mailbox_name, test_user2))
+    # helper.set_mailbox_rights(test_user1, knights_hall_mailbox_name, test_user2)
+    print('New rights: ')
+    print(helper.get_mailbox_rights(test_user1, knights_hall_mailbox_name, test_user2))
+    # print(helper.set_mailbox_aliases(test_user2, knights_hall_dict))
+
+
+if __name__ == '__main__':
+    # noinspection SpellCheckingInspection
+    full_rights = 'cdilrsw'
+    system_name: str = 'cgpro_credentials'
+    username: str = 'milokum.pavel@energospb.ru'
+    main_args = parse_arguments()
+    if main_args.newpass:
+        new_password: str = getpass.getpass(prompt='Enter password:')
+        try:
+            keyring.set_password(system_name, username, new_password)
+        except Exception as error:
+            print('Error: {}'.format(error))
+    else:
+        print(get_all_users_by_domains())
---
+++ server.py
@@ -0,0 +1,95 @@
+from flask import request, Flask, json, jsonify, abort, session
+import flask_cors
+import helper
+from werkzeug.exceptions import HTTPException
+import re
+from CGPCLI.Errors import FailedLogin
+
+
+app = Flask(__name__)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config.update(DEBUG=True,
+                  SECRET_KEY='3e33a45081c21b6b7a958094726f8b5c4aa6a098a71fc3c2782b5d2e59a18886',
+                  SESSION_COOKIE_HTTPONLY=True,
+                  REMEMBER_COOKIE_HTTPONLY=True,
+                  SESSION_COOKIE_SAMESITE='Strict',
+                  )
+# app.secret_key = '3e33a45081c21b6b7a958094726f8b5c4aa6a098a71fc3c2782b5d2e59a18886'
+flask_cors.CORS(app, supports_credentials=True)
+
+regex = re.compile(r'[\w.]+@\w+\.ru')
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+
+# @app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
+
+
+@app.route('/')
+def hello_world():
+    return '<p>Hello, World!</p>'
+
+
+@app.route('/api/users', methods=['GET'])
+def all_users():
+    users = None
+    if 'username' not in session:
+        abort(401)
+    mail_serv: helper.CGPROHelper = helper.CGPROHelper(session['username'], session['password'])
+    try:
+        users = mail_serv.get_all_users_by_domains()
+    except TimeoutError:
+        abort(500, description="Timeout ")
+    except FailedLogin:
+        abort(401)
+    return jsonify(users)
+
+
+@app.route('/api/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return jsonify({'http:200'})
+
+
+@app.route('/api/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if 'username' in session:
+            return jsonify({'http': 200, 'username': session['username']})
+        else:
+            abort(401)
+    if request.method == 'POST':
+        data = request.json
+        domains = None
+        if ('username' in data) and ('password' in data) and (re.fullmatch(regex, data['username'])):
+            mail_server: helper.CGPROHelper = helper.CGPROHelper(data['username'], data['password'])
+            try:
+                domains = mail_server.get_domains()
+            except TimeoutError:
+                abort(500, description="Timeout ")
+            except FailedLogin:
+                abort(401)
+            if domains:
+                session['username'] = data['username']
+                session['password'] = data['password']
+                return jsonify({'http': 200, 'username': session['username']})
+        abort(401)
+
+
+if __name__ == '__main__':
+    app.run()
